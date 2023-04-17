8a1cc1730e833445986378edbb84db3f5933d950
Van Vu <vanvn.vu@gmail.com>
2023-04-17 10:51:57 +0700

    Added Map

---
+++ map.ts
@@ -0,0 +1,10 @@
+let map = new Map();
+map.set("22", "Infosys");
+map.set(32, "TSC");
+map.set(false, "Google");
+map.set("45", "Facebook");
+map.set("65", "Amazon");
+
+// console.log(map);
+// console.log(map.get(false));
+console.log(map.get("65"));
